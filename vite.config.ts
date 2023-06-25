16e2369ddea045df9b267ffb19d2664bad7a3f83
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2023-06-25 10:08:16 +0700

    add enviroment

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,28 @@
 
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
-  },
-  server: {
-    port: 3000,
-  },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['./src/setupTests.ts'],
+    },
+    server: {
+      port: 3000,
+    },
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+  };
 });
