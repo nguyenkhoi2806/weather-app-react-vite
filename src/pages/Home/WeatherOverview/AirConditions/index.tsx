c9b616028435dfecec04386691b6f687414ef704
nguyenkhoi2806 <nhunguyennq@nashtechglobal.com>
2023-07-06 22:24:05 +0700

    add condition air

--- src/pages/Home/WeatherOverview/AirConditions/index.tsx
+++ src/pages/Home/WeatherOverview/AirConditions/index.tsx
@@ -1,22 +1,39 @@
+import { BiCloud } from 'react-icons/bi';
+import { CiTempHigh } from 'react-icons/ci';
+import { PiWindLight } from 'react-icons/pi';
+import { WiHumidity } from 'react-icons/wi';
+
 import Style from './air-conditions.module.scss';
 
 const AirCondition = () => {
   return (
     <div className={Style['air-condition']}>
       <div className={Style['air-condition__item']}>
-        <span className={Style['air-condition__item-title']}>Real Feel</span>
+        <span className={Style['air-condition__item-title']}>
+          <CiTempHigh size={25} className="mr-2" />
+          Real Feel
+        </span>
         <span className={Style['air-condition__item-content']}>19 °C</span>
       </div>
       <div className={Style['air-condition__item']}>
-        <span className={Style['air-condition__item-title']}>Wind</span>
+        <span className={Style['air-condition__item-title']}>
+          <PiWindLight size={25} className="mr-2" />
+          Wind
+        </span>
         <span className={Style['air-condition__item-content']}>0.45 m/s</span>
       </div>
       <div className={Style['air-condition__item']}>
-        <span className={Style['air-condition__item-title']}>Clouds</span>
+        <span className={Style['air-condition__item-title']}>
+          <BiCloud size={25} className="mr-2" />
+          Clouds
+        </span>
         <span className={Style['air-condition__item-content']}>96 %</span>
       </div>
       <div className={Style['air-condition__item']}>
-        <span className={Style['air-condition__item-title']}>Humidity</span>
+        <span className={Style['air-condition__item-title']}>
+          <WiHumidity size={25} className="mr-2" />
+          Humidity
+        </span>
         <span className={Style['air-condition__item-content']}>82 %</span>
       </div>
     </div>
