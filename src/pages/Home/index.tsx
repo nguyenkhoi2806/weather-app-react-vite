fabd4ddcd5cc80d3b609a1b8ec5d3b871e1ed11c
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2023-07-08 22:40:14 +0700

    add county select

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,5 @@
+import Select from 'react-select';
+
 import Header from './Header';
 import StyleHomes from './home.module.scss';
 import WeatherOverview from './WeatherOverview';
@@ -6,6 +8,7 @@ const Home = () => {
   return (
     <div className={StyleHomes['home-container']}>
       <Header />
+      <Select className="my-5" />
       <WeatherOverview />
     </div>
   );
